7d1fad97ba058515e609d97d4b0f212807b41c70
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-03-09 15:59:11 -0500

    update

--- commands/newtrade.js
+++ commands/newtrade.js
@@ -10,6 +10,7 @@ module.exports = {
         // interaction.member is the GuildMember object, which represents the user in the specific guild
         //const fromUser = interaction.user.username;
        //const toUser = interaction.user.username;
+       //this is a new comment
 
         await interaction.reply(`Starting trade... thank you ${interaction.user.username}`);
 
