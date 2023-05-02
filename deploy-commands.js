934805a1c84d03f0d2d77a0426112a76f38da538
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-02 16:24:11 -0400

    test
    test

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,7 +10,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles)
 {
 	const command = require(`./commands/${file}`);
-	if (commands.data != undefined)
+	if (command.data != undefined)
 	{
 	commands.push(command.data.toJSON());
 	}
