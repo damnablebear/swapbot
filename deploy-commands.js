215e9f0bf92ac1ed2b649b4a55df2ede2b3da6df
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-02 16:00:05 -0400

    test
    test

--- deploy-commands.js
+++ deploy-commands.js
@@ -7,7 +7,8 @@ const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
+for (const file of commandFiles)
+{
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
@@ -16,18 +17,28 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
-(async () => {
-	try {
+(async () =>
+{
+	try
+	{
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
+		try
+		{
+			const data = await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: commands },
+			);
+		}
+		catch (ex)
+		{
+			console.log("uh oh here's the breakpoint")
+		}
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
+	} catch (error)
+	{
 		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
@@ -38,7 +49,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 //	.then(() => console.log('Successfully deleted guild command'))
 //	.catch(console.error);
 
- //for global commands
+//for global commands
 rest.delete(Routes.applicationCommand(clientId, '1077344956315869304'))
 	.then(() => console.log('Successfully deleted application command'))
 	.catch(console.error);
