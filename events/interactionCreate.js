463a316218fd55ceaf8c43addb1941e4d07c279a
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-04 11:27:27 -0400

    test
    test

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -32,31 +32,7 @@ module.exports = {
 
 
 
-        if (interaction.isChatInputCommand()) 
-        {
-            console.log("interaction is: " + interaction);
-            console.log("interaction client is: " + interaction.client);
-            console.log("interaction client commands is " + interaction.client.commands);
-            console.log("interaction commandname is: " + interaction.commandName);
-            const command = interaction.client.commands.get(interaction.commandName);
-
-            if (!command)
-            {
-                console.error(`No command matching ${interaction.commandName} was found.`);
-                return;
-            }
-
-            try
-            {
-                console.log("we're awaiting the command execution");
-                await command.execute(interaction, interaction.client);
-            } catch (error)
-            {
-                console.error(`Error executing ${interaction.commandName}`);
-                console.error(error);
-            }
-        }
-        else if (interaction.isAutocomplete())
+        if (interaction.isAutocomplete())
         {
             console.log("we're in the autocomplete block");
             console.log("interaction is: " + interaction);
@@ -80,6 +56,38 @@ module.exports = {
                 console.error(`Error executing ${interaction.commandName}`);
                 console.error(error);
             }
+        }
+        else if (interaction.isChatInputCommand()) 
+        {
+            console.log("interaction is: " + interaction);
+            console.log("interaction client is: " + interaction.client);
+            console.log("interaction client commands is " + interaction.client.commands);
+            console.log("interaction commandname is: " + interaction.commandName);
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command)
+            {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try
+            {
+                console.log("we're awaiting the command execution");
+                await command.execute(interaction, interaction.client);
+            } catch (error)
+            {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+        }
+        else if (interaction.isButton())
+        {
+            //
+        }
+        else if (interaction.isStringSelectMenu())
+        {
+            //
         }
         else
         {
