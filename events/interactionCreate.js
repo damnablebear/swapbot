bbe6c3c1ee6a1ffb12d089c991f3ec6f2cc62820
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-04 10:50:55 -0400

    test
    test

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,32 +2,65 @@ module.exports = {
     name: 'interactionCreate',
     async execute(interaction)
     {
-        if (!interaction.isChatInputCommand()) 
+        /* if (!interaction.isChatInputCommand()) 
+         {
+             console.log("we're getting in here because this isn't a chat input command");
+             return;
+         }
+ 
+         console.log("interaction is: " + interaction);
+         console.log("interaction client is: " + interaction.client);
+         console.log("interaction client commands is " + interaction.client.commands);
+         console.log("interaction commandname is: " + interaction.commandName);
+         const command = interaction.client.commands.get(interaction.commandName);
+ 
+         if (!command)
+         {
+             console.error(`No command matching ${interaction.commandName} was found.`);
+             return;
+         }
+ 
+         try
+         {
+             console.log("we're awaiting the command execution");
+             await command.execute(interaction, interaction.client);
+         } catch (error)
+         {
+             console.error(`Error executing ${interaction.commandName}`);
+             console.error(error);
+         }*/
+
+
+
+        if (interaction.isChatInputCommand()) 
+        {
+            console.log("interaction is: " + interaction);
+            console.log("interaction client is: " + interaction.client);
+            console.log("interaction client commands is " + interaction.client.commands);
+            console.log("interaction commandname is: " + interaction.commandName);
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command)
+            {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try
+            {
+                console.log("we're awaiting the command execution");
+                await command.execute(interaction, interaction.client);
+            } catch (error)
+            {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+        }
+        else
         {
             console.log("we're getting in here because this isn't a chat input command");
             return;
         }
-        
-        console.log("interaction is: " + interaction);
-        console.log("interaction client is: " + interaction.client);
-        console.log("interaction client commands is " + interaction.client.commands);
-        console.log("interaction commandname is: " + interaction.commandName);
-        const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command)
-        {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-
-        try
-        {
-            console.log("we're awaiting the command execution");
-            await command.execute(interaction, interaction.client);
-        } catch (error)
-        {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
-        }
     },
 };
