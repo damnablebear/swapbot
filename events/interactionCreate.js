35843a0102d44581e69a266c0427408b00718406
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-04 10:52:48 -0400

    test
    test

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -56,6 +56,31 @@ module.exports = {
                 console.error(error);
             }
         }
+        else if (interaction.isAutocomplete())
+        {
+            console.log("we're in the autocomplete block");
+            console.log("interaction is: " + interaction);
+            console.log("interaction client is: " + interaction.client);
+            console.log("interaction client commands is " + interaction.client.commands);
+            console.log("interaction commandname is: " + interaction.commandName);
+            const command = interaction.client.commands.get(interaction.commandName);
+
+            if (!command)
+            {
+                console.error(`No command matching ${interaction.commandName} was found.`);
+                return;
+            }
+
+            try
+            {
+                console.log("we're awaiting the command execution");
+                await command.autocomplete(interaction);
+            } catch (error)
+            {
+                console.error(`Error executing ${interaction.commandName}`);
+                console.error(error);
+            }
+        }
         else
         {
             console.log("we're getting in here because this isn't a chat input command");
