6934b42d51d1c0397bf742dc84692b168bf0acd3
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-04 15:24:34 -0400

    test
    test

--- commands/addTracking.js
+++ commands/addTracking.js
@@ -1,32 +1,40 @@
 const { ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 
+/* 
 
-const trackModal = new ModalBuilder()
-    //this custom id is one that we can use to define all incoming interactions
-    .setCustomId('trackingModal')
-    .setTitle('Tracking Modal');
+idk what i'm doing here
 
-// Create the text input components
-const trackingProvInput = new TextInputBuilder()
-    .setCustomId('trackingProvInput')
-    // The label is the prompt the user sees for this input
-    .setLabel("What tracking provider are you sending with?")
-    // Short means only a single line of text
-    // Paragraph means multiple lines of text.
-    .setStyle(TextInputStyle.Short);
 
-const trackingNumInput = new TextInputBuilder()
-    .setCustomId('trackingNumInput')
-    .setLabel("What's the tracking number?")
-    .setStyle(TextInputStyle.Short);
+async execute(interaction) {
+    const trackModal = new ModalBuilder()
+        //this custom id is one that we can use to define all incoming interactions
+        .setCustomId('trackingModal')
+        .setTitle('Tracking Modal')
 
-// An action row only holds one text input,
-// so you need one action row per text input.
-const firstActionRow = new ActionRowBuilder().addComponents(trackingProvInput);
-const secondActionRow = new ActionRowBuilder().addComponents(trackingNumInput);
+    // Create the text input components
+    const trackingProvInput = new TextInputBuilder()
+        .setCustomId('trackingProvInput')
+        // The label is the prompt the user sees for this input
+        .setLabel("What tracking provider are you sending with?")
+        // Short means only a single line of text
+        // Paragraph means multiple lines of text.
+        .setStyle(TextInputStyle.Short);
 
-// Add inputs to the modal
-modal.addComponents(firstActionRow, secondActionRow);
+    const trackingNumInput = new TextInputBuilder()
+        .setCustomId('trackingNumInput')
+        .setLabel("What's the tracking number?")
+        .setStyle(TextInputStyle.Short);
 
-// Show the modal to the user
-await interaction.showModal(trackModal);
+    // An action row only holds one text input,
+    // so you need one action row per text input.
+    const firstActionRow = new ActionRowBuilder().addComponents(trackingProvInput);
+    const secondActionRow = new ActionRowBuilder().addComponents(trackingNumInput);
+
+    // Add inputs to the modal
+    modal.addComponents(firstActionRow, secondActionRow);
+
+    // Show the modal to the user
+    await interaction.showModal(trackModal);
+}
+
+*/
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -70,52 +70,7 @@ module.exports = {
                 console.error(`No command matching ${interaction.commandName} was found.`);
                 return;
             }
-            //catching Modals
 
-            if (interaction.commandName === 'addTracking')
-            {
-                const trackModal = new ModalBuilder()
-                    //this custom id is one that we can use to define all incoming interactions
-                    .setCustomId('trackingModal')
-                    .setTitle('Tracking Modal');
-
-                // Create the text input components
-                const trackingProvInput = new TextInputBuilder()
-                    .setCustomId('trackingProvInput')
-                    // The label is the prompt the user sees for this input
-                    .setLabel("What tracking provider are you sending with?")
-                    // Short means only a single line of text
-                    // Paragraph means multiple lines of text.
-                    .setStyle(TextInputStyle.Short);
-
-                const trackingNumInput = new TextInputBuilder()
-                    .setCustomId('trackingNumInput')
-                    .setLabel("What's the tracking number?")
-                    .setStyle(TextInputStyle.Short);
-
-                // An action row only holds one text input,
-                // so you need one action row per text input.
-                const firstActionRow = new ActionRowBuilder().addComponents(trackingProvInput);
-                const secondActionRow = new ActionRowBuilder().addComponents(trackingNumInput);
-
-                // Add inputs to the modal
-                modal.addComponents(firstActionRow, secondActionRow);
-
-                // Show the modal to the user
-                await interaction.showModal(trackModal);
-            }
-
-            console.log("we're in a modal menu interaction");
-            console.log("interaction is: " + interaction);
-            console.log("modal customid value is: " + interaction.customId());
-            const modalId = interaction.customId();
-
-
-            if (modalId === 'trackingModal')
-            {
-                console.log("success you did it gj gj gj");
-
-            }
             try
             {
                 console.log("we're awaiting the command execution");
@@ -157,6 +112,35 @@ module.exports = {
             // check if the interaction is a userSelectMenu
             const selectedValue = interaction.values[0]; // get the selected value
             interaction.reply('this is a user select menu. the selected value is ' + selectedValue); // send an error message
+        }
+        //catching Modals
+        else if (interaction.isModalSubmit())
+        {
+            /* maybe garbage idk
+            if (interaction.commandName === 'addTracking')
+            {
+                const trackModal = new ModalBuilder()
+                    //this custom id is one that we can use to define all incoming interactions
+                    .setCustomId('trackingModal')
+                    .setTitle('Tracking Modal');
+
+                    // Show the modal to the user
+            await interaction.showModal(trackModal);
+            }
+
+            console.log("we're in a modal menu interaction");
+            console.log("interaction is: " + interaction);
+            console.log("modal customid value is: " + interaction.customId());
+            const modalId = interaction.customId();
+
+
+            if (modalId === 'trackingModal')
+            {
+                console.log("success you did it gj gj gj");
+
+            }
+
+            */
         }
         else
         {
