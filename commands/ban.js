e9686808aa245066e06c5587c322b4729b6a660b
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-04 11:13:22 -0400

    test
    test

--- commands/ban.js
+++ commands/ban.js
@@ -118,6 +118,5 @@ module.exports = {
             await response.edit({ content: 'Confirmation not received within 60 seconds, cancelling', components: []});
         }
         //if the user is still in the guild where this command is run, you can also use .getMember('target') to get their GuildMember object
-        await interaction.guild.members.ban(target);
     }
 };
