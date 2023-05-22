c7e252aa85421bdcb40ad3ebc054b7a975663a76
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-22 12:05:28 -0400

    test
    test

--- commands/swapbotCommands/newtrade.js
+++ commands/swapbotCommands/newtrade.js
@@ -1,28 +1,44 @@
-const {SlashCommandBuilder, ActionRowBuilder, UserSelectMenuBuilder} = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, UserSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
-        name: 'newtrade',
-        description: 'Allows you to start a new trade with a user.',
+    data: new SlashCommandBuilder()
+        .setName('newtrade')
+        .setDescription('Allows you to start a new trade with a user.'),
     async execute(interaction)
     {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
         //const fromUser = interaction.user.username;
-       //const toUser = interaction.user.username;
-       //this is a new comment
-       const userSelect = new UserSelectMenuBuilder()
-       .setCustomId('newtrade')
-       .setPlaceholder('Select a user to trade with.')
-       .setMinValues(1)
-       .setMaxValues(1);
+        //const toUser = interaction.user.username;
+        //this is a new comment
+        const userSelect = new UserSelectMenuBuilder()
+            .setCustomId('newtrade')
+            .setPlaceholder('Select a user to trade with.')
+            .setMinValues(1)
+            .setMaxValues(1);
 
-   const row1 = new ActionRowBuilder()
-       .addComponents(userSelect);
+        const row1 = new ActionRowBuilder()
+            .addComponents(userSelect);
 
-   await interaction.reply({
-       content: "Select the user you'd like to trade with:",
-       components: [row1],
-   });
+        const response = await interaction.reply({
+            content: "Select the user you'd like to trade with:",
+            components: [row1],
+            ephemeral: true,
+
+        });
+        const collectorFilter = i => i.user.id === interaction.user.id;
+        const confirmation = await response.awaitMessageComponent({filter: collectorFilter, time: 60000});
+
+
+        try{
+
+            //now we are checking which button is clicked and sending an interaction to discord based on the button
+            await confirmation.update({ content: `You have started a trade with ${confirmation.users.first().username}, and your trade ID is: TRADE ID`, components: [] });
+        }
+        catch (e) {
+            console.log(e)
+            await response.edit({ content: 'User confirmation not received within 60 seconds, cancelling', components: []});
+        }
 
     },
 };
