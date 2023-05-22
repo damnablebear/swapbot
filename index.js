14f6d6a32a02e00a480d41080d2eeec503162121
Ryan Littlefield <ryanelittlefield@warm.coffee>
2023-05-22 12:13:25 -0400

    test
    test

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const eventHandler = require('./handlers/eventHandler')
-require("dotenv").config();
 
 // Create a new client instance
 //the term 'Guilds' is used by the discord api to refer to a discord server
@@ -13,7 +11,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 //the 'Client' class in discord.js extends the EventEmitter class, so the client object exposes the '.on()' and '.once()' methods that we are using below. 
 //the '.on()' and '.once()' methods take two arguments - the event name and a callback function
-/*const eventsPath = path.join(__dirname, 'events');
+const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles)
@@ -33,36 +31,35 @@ for (const file of eventFiles)
         client.on(event.name, (...args) => event.execute(...args));
     }
 
-    //console.log("filepath is: " + filePath);
+    console.log("filepath is: " + filePath);
 }
-*/
-eventHandler(client);
 
-//client.commands = new Collection();
+
+client.commands = new Collection();
 
 //setting a commands path with a dirname (which pulls from the currently-executing script) and joining the commands string/folder to the end
-//const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, 'commands');
 //reads and syncs the filesystem noted in commandspath, and filters out all JS files, leaving only those 
-//const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 
 //for each file left (all JS files in the commands folder at this point), we're joining the commands path to the file name to make a full filepath variable (filePath)
 //then we're stating that a 'command' variable requires a filepath (which makes sense, where else would it pull from)
 //then we're looking to see if "data" and "execute" are present in a given command, and pulling the command's name from the data section, and setting that as the command
-//for (const file of commandFiles)
-//{
-//    const filePath = path.join(commandsPath, file);
-//    const command = require(filePath);
-//    //Set a new item in the Collection with the key as the command name and the value as the exported module
-//    if ('data' in command && 'execute' in command)
-//    {
-//        client.commands.set(command.data.name, command);
-//    }
-//    else
-//  {
-//        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-//    }
-//}
+for (const file of commandFiles)
+{
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    //Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command)
+    {
+        client.commands.set(command.data.name, command);
+    }
+    else
+    {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
 
 // Log in to Discord with your client's token
 client.login(token);
